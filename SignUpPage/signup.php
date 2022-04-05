04bb5b24832d32dd5d8beec13155eb5e883f6a12
reinab2001 <reinabitar2014@gmail.com>
2022-04-05 18:42:00 +0300

    tried to fix login and signup footer

--- SignUpPage/signup.php
+++ SignUpPage/signup.php
@@ -5,135 +5,137 @@ include("../connection.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Sign Up</title>
-        <link href="css/styles.css" rel="stylesheet" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="bg-primary">
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
-                <main>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-7">
-                                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Create Account</h3></div>
-                                    <div class="card-body">
-                                        <form action="signupmethod.php" method="POST">
-                                            <div class="row mb-3">
-                                                <div class="col-md-6">
-                                                    <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" id="inputFirstName" type="text" name="first_name" placeholder="Enter your first name" Required />
-                                                        <label for="inputFirstName">First name</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-floating">
-                                                        <input class="form-control" id="inputLastName" type="text" name="last_name" placeholder="Enter your last name" Required />
-                                                        <label for="inputLastName">Last name</label>
-                                                    </div>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Sign Up</title>
+    <link href="vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous">
+    </script>
+</head>
+
+<body class="bg-primary">
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-7">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header">
+                                    <h3 class="text-center font-weight-light my-4">Create Account</h3>
+                                </div>
+                                <div class="card-body">
+                                    <form action="signupmethod.php" method="POST">
+                                        <div class="row mb-3">
+                                            <div class="col-md-6">
+                                                <div class="form-floating mb-3 mb-md-0">
+                                                    <input class="form-control" id="inputFirstName" type="text"
+                                                        name="first_name" placeholder="Enter your first name"
+                                                        Required />
+                                                    <label for="inputFirstName">First name</label>
                                                 </div>
                                             </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputEmail" type="email" name="email" placeholder="name@example.com" Required/>
-                                                <label for="inputEmail">Email address</label>
-                                            </div>
-                                            <div class="row mb-3">
-                                                <div class="col-md-6">
-                                                    <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" id="inputUsername" type="text" name="username" placeholder="Create a username" Required/>
-                                                        <label for="inputUsername">Username</label>
-                                                    </div>
+                                            <div class="col-md-6">
+                                                <div class="form-floating">
+                                                    <input class="form-control" id="inputLastName" type="text"
+                                                        name="last_name" placeholder="Enter your last name" Required />
+                                                    <label for="inputLastName">Last name</label>
                                                 </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" id="inputPassword" type="password" name= "password" placeholder="Create a password" Required/>
-                                                        <label for="inputPassword">Password</label>
-                                                    </div>
-                                                </div>
-                                                
                                             </div>
-                                            <div class="row mb-3">
-                                                <div class="col-md-6">
-                                                    <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" id="inputPhone" type="text" name="phone_nb" placeholder="Your phone number" Required/>
-                                                        <label for="inputPhone">Phone Number</label>
-                                                    </div>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" id="inputEmail" type="email" name="email"
+                                                placeholder="name@example.com" Required />
+                                            <label for="inputEmail">Email address</label>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <div class="col-md-6">
+                                                <div class="form-floating mb-3 mb-md-0">
+                                                    <input class="form-control" id="inputUsername" type="text"
+                                                        name="username" placeholder="Create a username" Required />
+                                                    <label for="inputUsername">Username</label>
                                                 </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" id="inputAddress" type="text" name="address" placeholder="Your address" Required/>
-                                                        <label for="inputAddress">Address</label>
-                                                    </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-floating mb-3 mb-md-0">
+                                                    <input class="form-control" id="inputPassword" type="password"
+                                                        name="password" placeholder="Create a password" Required />
+                                                    <label for="inputPassword">Password</label>
                                                 </div>
-                                                
                                             </div>
-                                            <div class="mt-4 mb-0">
-                                                <div class="d-grid"><button class="btn bot btn-block"  name = 'submit' type='submit'>Create Account</button></div>
+
+                                        </div>
+                                        <div class="row mb-3">
+                                            <div class="col-md-6">
+                                                <div class="form-floating mb-3 mb-md-0">
+                                                    <input class="form-control" id="inputPhone" type="text"
+                                                        name="phone_nb" placeholder="Your phone number" Required />
+                                                    <label for="inputPhone">Phone Number</label>
+                                                </div>
                                             </div>
-                                        </form>
-                                    </div>
-                                    <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="login.php">Have an account? Go to login</a></div>
-                                    </div>
+                                            <div class="col-md-6">
+                                                <div class="form-floating mb-3 mb-md-0">
+                                                    <input class="form-control" id="inputAddress" type="text"
+                                                        name="address" placeholder="Your address" Required />
+                                                    <label for="inputAddress">Address</label>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div class="mt-4 mb-0">
+                                            <div class="d-grid"><button class="btn bot btn-block" name='submit'
+                                                    type='submit'>Create Account</button></div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="card-footer text-center py-3">
+                                    <div class="small"><a href="login.php">Have an account? Go to login</a></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </main>
-            </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light">
-                    <div class="container2 container-fluid px-4">
-                       
-                        <div class="social">
-                            <a class="insta" href="https://www.instagram.com/_reiinaaa_/" target="_blank">
-                                <i class="fab fa-instagram"></i></a>
-                            <a class="twitter" href="https://twitter.com/_reinaaaa_" target="_blank">
-                                    <i class="fab fa-twitter"></i></a>
-                            <a class="facebook" href="" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                            
-                
-                        </div>
-                    
-                
-                    
-                    <div class="info">
-                        <p class="us">About us</p>
-                        <div class="info1 infoAll">
-                            <i class="fas fa-phone telephone"></i>
-                            <p class="num">+961 03 666 666</p>
-                        </div>
-                        <div class="info2 infoAll">
-                            <i class="far fa-clock time"></i>
-                            <p class="sched">Mon-Sat: 8AM TILL 9PM</p>
-                        </div>
-                        <div class="info3 infoAll">
-                            <i class="fa fa-map-marker-alt loc"></i>
-                            <p class="spot">Beirut-Hamra Street</p>
-                        </div>
-                    </div>
-                    <div class="em">
-                        <a class="email" href="mailto: reina.bitar@lau.edu" target="_blank"><i
-                            class="far fa-envelope"></i></a>
-                            <p class="mymail">reina.bitar@lau.edu</p>
-                    </div>
-                    </div>
-                
-                </footer>
-            </div>
+                </div>
+            </main>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-    </body>
-    
+        <div id="layoutAuthentication_footer">
+            <footer class="py-4 bg-light" id="footer">
+                <div class="container px-4">
+
+                    <h3>Purr-fect pets</h3>
+                    <p>People trust us, pets love us.</p>
+                    <div class="social-links">
+                        <a href="https://twitter.com/_reinaaaa_" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="https://www.instagram.com/_reiinaaa_/" class="instagram"><i
+                                class="bx bxl-instagram"></i></a>
+                        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                    </div>
+                    <div class="copyright">
+                        &copy; Copyright <strong><span>Purr-fect pets</span></strong>. All Rights Reserved
+                    </div>
+                </div>
+
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+    <script src="js/scripts.js"></script>
+</body>
+
 </html>
